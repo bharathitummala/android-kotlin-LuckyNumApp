59c8ff56fcc94d3c2434276bd6ec53ba19879130
Bharathi Tummala <bharathi.tummala87@gmail.com>
2024-03-13 13:53:41 -0700

    added intent and extras

--- app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
+++ app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -2,10 +2,24 @@ package com.example.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 
-class LuckyNumberActivity : AppCompatActivity() {
+class LuckyNumberActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_number)
+        val resTxt : TextView = findViewById(R.id.resTxt)
+        val luckyTxt:TextView =findViewById(R.id.LuckyTxtNum)
+        val shareBtn : Button = findViewById(R.id.shareBtn)
+       val user_name = recveUserName()
+        Toast.makeText(this, "Hello "+user_name, Toast.LENGTH_LONG).show()
+    }
+    fun recveUserName():String{
+        val bundle:Bundle? = intent.extras
+        val usrName = bundle?.get("name").toString()
+        return usrName
     }
 }
--- app/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.luckynumber
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,4 +21,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }}
+        val b1 :Button = findViewById(R.id.wishBtn)
+    //    val welTxt: TextView = findViewById(R.id.welTxt)
+        val etText : EditText = findViewById(R.id.etTxt)
+
+        b1.setOnClickListener(){
+            val userName = etText.text
+
+            val i = Intent(this, LuckyNumberActivity::class.java)
+
+            i.putExtra("name", userName)
+            startActivity(i)
+        }
+
+    }
+}
