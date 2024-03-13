937d86645e914c51b2578a2ba37764906bb6df4c
Bharathi Tummala <bharathi.tummala87@gmail.com>
2024-03-13 13:09:25 -0700

    added new activity

---
+++ app/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -0,0 +1,11 @@
+package com.example.luckynumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LuckyNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lucky_number)
+    }
+}
--- app/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -15,32 +15,5 @@ import com.example.luckynumber.ui.theme.LuckyNumberTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LuckyNumberTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LuckyNumberTheme {
-        Greeting("Android")
-    }
-}
+        setContentView(R.layout.activity_main)
+    }}
